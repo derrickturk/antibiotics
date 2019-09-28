9f0a40b9089164383527fd3e2c01e5978e4e5574
derrickturk <dwt@terminusdatascience.com>
2019-09-28 14:51:50 -0500

    Author info in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from distutils.core import setup
 
 setup(
     name='antibiotics',
+    description='a treament for PANDAS',
+    author='Derrick W. Turk',
+    url='https://github.com/derrickturk/antibiotics',
     version='0.1',
     py_modules=['antibiotics'],
 )
